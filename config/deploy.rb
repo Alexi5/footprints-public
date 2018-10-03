05074c86c6152b9bb2e800169168907bc530274d
Tim Rourke <tim@timrourke.com>
2018-10-03 17:06:27 -0500

    Share sockets and pids

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ append :linked_files, "config/database.yml"
 append :linked_files, "config/mailer.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/cache", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/sockets", "tmp/cache", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
