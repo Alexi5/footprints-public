f28819c4df5d24032ee5d2dd78eabdbd9692933e
Joshua Bartlett <joshua.r.bartlett@gmail.com>
2018-10-17 05:45:55 -0500

    Travis 10

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -4,6 +4,6 @@ openssl aes-256-cbc -k $DEPLOY_KEY \
   -in config/deploy_id_rsa_enc_travis \
   -out config/deploy_id_rsa -d -a
 
-bundle install --path=${BUNDLE_PATH:-vendor/bundle}
+bundle install
 
-bundle exec cap production deploy --path=${BUNDLE_PATH:-vendor/bundle}
+bundle exec cap production deploy
