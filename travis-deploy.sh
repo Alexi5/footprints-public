84577a3ec486c6788e142e88146f7c7a2dea1e80
Joshua Bartlett <joshua.r.bartlett@gmail.com>
2018-10-17 06:46:43 -0500

    Travis 16

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 openssl aes-256-cbc -k $DEPLOY_KEY \
-  -in config/deploy_id_rsa_enc_travis \
-  -out config/deploy_id_rsa -d -a
+  -in ./config/deploy_id_rsa_enc_travis \
+  -out ./config/deploy_id_rsa -d -a
 
 bundle install --without development test
 
