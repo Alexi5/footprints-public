c7f0c48947a69beb048f3e2dbc6e85c9fe2e84a2
Joshua Bartlett <joshua.r.bartlett@gmail.com>
2018-10-17 07:05:08 -0500

    travis 1000

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -4,6 +4,8 @@ openssl aes-256-cbc -k $DEPLOY_KEY \
   -in ./config/deploy_id_rsa_enc_travis \
   -out ./config/deploy_id_rsa -d -a
 
+chmod 600 ./config/deploy_id_rsa
+
 bundle install --without development test
 
 bundle exec cap production deploy
